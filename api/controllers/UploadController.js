0de43450b6c047b32306059598e7b2614f935c2a
guillermo bascunana soler <guillermobascunanasoler@MacBook-Pro-de-guillermo.local>
2018-05-08 22:47:48 +0200

    excel process

--- api/controllers/UploadController.js
+++ api/controllers/UploadController.js
@@ -4,10 +4,20 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
+convert = require('excel-as-json').processFile;
+
+const options = {
+
+}
+
+
 module.exports = {
     upload: (req,res)=>{
       req.file('file').upload(function (err, uploadedFiles) {
-        res.send();
+        convert(uploadedFiles[0].fd, null, options,function(err, output) {
+          console.log('succes');
+          res.send();
+        });
       });
     }
 };
