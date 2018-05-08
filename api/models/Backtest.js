096c4a4cba8c17e4ee1509a2d3e5607824cd4d97
guillermo bascunana soler <guillermobascunanasoler@MacBook-Pro-de-guillermo.local>
2018-05-08 23:40:26 +0200

    building model

--- api/models/Backtest.js
+++ api/models/Backtest.js
@@ -4,11 +4,26 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+const Schema = require('mongoose').Schema;
 
-module.exports = {
+var Backtest = new Schema({
+  ticker: {type:String, required:true},
+  nombre: String,
+  isin: String,
+  year: Number,
+  pais: String,
+  margenBeneficios: Number,
+  margenBeneficioMinus1: Number,
+  momentum: Number,
+  momentum: Number,
+  totalAssets: Number,
+  valentum: Number,
+  valqual: Number,
+  value: Number,
+  volatilidad260: Number
+},{
+  collection: 'backtest'
+});
 
-  attributes: {
-
-  }
-};
+module.exports = Backtest;
 
--- config/datastores.js
+++ config/datastores.js
@@ -14,8 +14,6 @@
  */
 
 module.exports.datastores = {
-
-
   /***************************************************************************
   *                                                                          *
   * Your app's default datastore.                                            *
@@ -31,27 +29,10 @@ module.exports.datastores = {
   * (For production configuration, see `config/env/production.js`.)          *
   *                                                                          *
   ***************************************************************************/
-
   default: {
-
-    /***************************************************************************
-    *                                                                          *
-    * Want to use a different database during development?                     *
-    *                                                                          *
-    * 1. Choose an adapter:                                                    *
-    *    https://sailsjs.com/plugins/databases                                 *
-    *                                                                          *
-    * 2. Install it as a dependency of your Sails app.                         *
-    *    (For example:  npm install sails-mysql --save)                        *
-    *                                                                          *
-    * 3. Then pass it in, along with a connection URL.                         *
-    *    (See https://sailsjs.com/config/datastores for help.)                 *
-    *                                                                          *
-    ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
-
+    adapter: 'sails-mongo',
+    url: 'localhost',
+    port: 27017,
+    database:'testBolsa'
   },
-
-
 };
