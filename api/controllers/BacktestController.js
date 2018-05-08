b9b250d009189d3d54b4c95ee04f8f50274f0105
guillermo bascunana soler <guillermobascunanasoler@MacBook-Pro-de-guillermo.local>
2018-05-08 23:07:48 +0200

    added mongoose

---
+++ api/controllers/BacktestController.js
@@ -0,0 +1,11 @@
+/**
+ * BacktestController
+ *
+ * @description :: Server-side logic for managing backtests
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
